21ce21c1c090345b85a509c5d06ca4105a1fc6d1
Deniz Tulay <denizlevent@Mac.fritz.box>
2025-12-31 15:18:47 +0100

    Fix: Add BrowserRouter to main.jsx for React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 // index.css moved to inline in index.html to avoid render-blocking
 
@@ -17,5 +18,7 @@ if ('serviceWorker' in navigator) {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
